38a2ac8788d817e20c66f5a01a12ce6ee19cdc47
douglasbryan94 <douglas.bryan94@gmail.com>
2020-03-07 01:18:34 -0500

    Designed page and developed the login action
    - Designed Login Page
    - Created Stored Procedure used to verify login credentials
    - Developed Login action inside controller

---
+++ ReserveIt/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReserveIt.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(string email, string password)
+        {
+            using (ReserveItEntities ent = new ReserveItEntities())
+            {
+                User result = ent.VerifyUserLogin(email, password).FirstOrDefault();
+
+                if (result != null)
+                {
+                    Session["userID"] = result.UserID;
+                    Session["email"] = result.Email;
+                    Session["password"] = result.Password;
+                    Session["accessLevel"] = result.UserLevel;
+                    Session["firstName"] = result.Firstname;
+                    Session["middleName"] = result.Middlename;
+                    Session["lastName"] = result.Lastname;
+                    Session["streetAddress"] = result.StreetAddress;
+                    Session["cityAddress"] = result.CityAddress;
+                    Session["stateAddress"] = result.StateAddress;
+                    Session["countryAddress"] = result.CountryAddress;
+                    Session["zipAddress"] = result.ZIPAddress;
+                    Session["phone"] = result.Phone;
+
+                    return RedirectToAction("Index", "Main");
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
